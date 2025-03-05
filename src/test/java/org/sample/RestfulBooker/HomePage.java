de121b5de521882294d27b50fa8b437dd8135976
Nick <gex1040@hotmail.com>
2025-03-04 17:03:50 -0800

    Added in unit tests for verifying description and images.

--- src/test/java/org/sample/RestfulBooker/HomePage.java
+++ src/test/java/org/sample/RestfulBooker/HomePage.java
@@ -1,17 +1,24 @@
 package org.sample.RestfulBooker;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 import java.lang.invoke.MethodHandles;
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -30,6 +37,7 @@ class HomePage {
 	@BeforeEach
 	void setUp() throws Exception {
 		driver = new ChromeDriver();
+		driver.get(sutURL);
 	}
 
 	@AfterEach
@@ -38,8 +46,46 @@ class HomePage {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void Homepage_image_is_logo() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement logo = wait.until(
+				ExpectedConditions.presenceOfElementLocated(By.className("hotel-logoUrl")));
+		assertThat(logo.getDomAttribute("src")).containsIgnoringCase("logo");
 	}
-
+	
+	@Test
+	void Website_description_displays_on_homepage()
+	{	
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			WebElement logo = wait.until(
+					ExpectedConditions.presenceOfElementLocated(By.className("hotel-logoUrl")));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		String script = "arguments[0].scrollIntoView();";
+		js.executeScript(script, logo);
+		WebElement description = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[2]/p"));
+		String descriptionText = description.getText();
+		log.debug("Description text: {}", descriptionText);
+		assertThat(descriptionText).contains("Welcome to Shady Meadows");
+		
+	}
+	
+	@Test
+	void Room_description_displays_on_homepage()
+	{
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement description = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[4]/div/div/div[3]/p"));
+		String descriptionText = description.getText();
+		assertThat(descriptionText).isNotEmpty();
+	}
+	
+	@Test
+	void Room_image_is_displayed()
+	{
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			WebElement image= wait.until(
+					ExpectedConditions.presenceOfElementLocated(By.className("hotel-img")));
+			log.debug("Room image file name: {}", image.getDomAttribute("src"));
+			assertThat(image.getDomAttribute("src")).containsIgnoringCase("room");
+		
+	}
 }
